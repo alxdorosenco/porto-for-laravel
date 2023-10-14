db35b878ef367567d773538233083e53e59c82bf
Alexei Dorosenco <alexforever1808@gmail.com>
2023-10-14 21:20:33 +0300

    Owerwrite load method from console kernel

---
+++ src/Commands/Traits/ConsoleKernel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AlxDorosenco\PortoForLaravel\Commands\Traits;
+
+use AlxDorosenco\PortoForLaravel\Traits\FilesAndDirectories;
+use Illuminate\Console\Application as Artisan;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use Symfony\Component\Finder\Finder;
+
+trait ConsoleKernel
+{
+    use FilesAndDirectories;
+
+    /**
+     * Register all of the commands in the given directory.
+     *
+     * @param  array|string  $paths
+     * @return void
+     */
+    protected function load($paths)
+    {
+        $paths = array_unique(Arr::wrap($paths));
+
+        $paths = array_filter($paths, function ($path) {
+            return is_dir($path);
+        });
+
+        if (empty($paths)) {
+            return;
+        }
+
+        $namespace = $this->getNamespaceFromPath(config('porto.path')).'\\';
+
+        foreach ((new Finder)->in($paths)->files() as $command) {
+            $command = $namespace.str_replace(
+                    ['/', '.php'],
+                    ['\\', ''],
+                    Str::after($command->getRealPath(), realpath(config('porto.root')).DIRECTORY_SEPARATOR)
+                );
+
+            if (is_subclass_of($command, Command::class) &&
+                ! (new ReflectionClass($command))->isAbstract()) {
+                Artisan::starting(function ($artisan) use ($command) {
+                    $artisan->resolve($command);
+                });
+            }
+        }
+    }
+}
